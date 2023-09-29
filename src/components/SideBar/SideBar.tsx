779f347723cb45c3cd0177e973b25fb2fb7ae8ae
unknown <phamcaoky2001@gmail.com>
2023-09-29 17:48:30 +0700

    feat: create sidebar and header

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,78 @@
+import { UserOutlined, EditOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("List users", "1"),
+    getItem("My info", "2"),
+  ]),
+  getItem("Todo", "sub2", <EditOutlined />, [
+    getItem("List Todo", "6"),
+    getItem("Create Todo", "8"),
+  ]),
+];
+export default function SideBar() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2023 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+}
